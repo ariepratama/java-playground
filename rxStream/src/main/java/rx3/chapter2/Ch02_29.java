b247e9ce7abbed53e1185b1dd9351eafcf3789fa
Arie Pratama Sutiono <arie.pratama@ninjavan.co>
2020-04-23 10:20:46 +0800

    add singles

---
+++ rxStream/src/main/java/rx3/chapter2/Ch02_29.java
@@ -0,0 +1,26 @@
+package rx3.chapter2;
+
+import io.reactivex.Observable;
+import io.reactivex.Single;
+
+public class Ch02_29 {
+
+  public static void main(String args[]){
+    // single is an observable that only emit 1 output
+    Single.just("yellow")
+        .map(String::length)
+        .subscribe(System.out::println);
+
+    // calling Observable.first will return single
+    // because single needs to emit 1 element, then there's a default item to be returned
+    // in case it's empty
+    Observable<String> source = Observable.just("one", "two", "three");
+    source.first("None")
+        .subscribe(System.out::println);
+
+    source = Observable.empty();
+    source.first("No El")
+        .subscribe(System.out::println);
+  }
+
+}
